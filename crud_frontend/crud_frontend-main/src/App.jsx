87962eaf56890622e86217b326b505d55a1b3ff2
jayanthraj5 <2300032469@kluniversity.in>
2025-08-08 20:58:07 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://13.223.81.84:9090/springapp1';
+const BASE_URL = 'http://98.86.244.56:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -49,7 +49,7 @@ const BASE_URL = 'http://13.223.81.84:9090/springapp1';
  
   const deleteProduct = async (id) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
-      await axios.delete(`http://13.223.81.84:9090/springapp1/delete/${id}`);
+      await axios.delete(`http://98.86.244.56:9090/springapp1/delete/${id}`);
       alert('Delete successful');
       fetchProducts();
     }
